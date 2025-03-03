fe4858a07f9d6f02386683a5baec86b5b7abb2da
Tanvee007 <tanveetyagi007@gmail.com>
2025-03-03 11:48:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElement("contact for m").addEventListner(
+    "submit,function"(Event){
+        event,preventDefault();
+        alert("Thank you! Your message has been sent.");
+    }
+)
